ec844cdb4c21abe59db7c4cca0fe956c8f7f8b6a
rohhj622 <rohhj622@gmail.com>
2022-03-03 16:52:51 +0900

    my_setting.py :220303

---
+++ snsP/my_settings.py
@@ -0,0 +1,37 @@
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ownway',
+#         'USER': 'ownway',
+#         'PASSWORD': '1Wkfkgkw!',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ownway',
+        'USER': 'ownway',
+        'PASSWORD': '1Wkfgkwk!',
+        'HOST': '15.164.49.223',
+        'PORT': '5432',
+    }
+}
+
+SECRET = {
+    'secret': 'django-insecure-086q_^v+nmmrrxmv$tb-q2qy)y0@rv*qfywtxfqe72xil0)6he',
+}
+
+SOCIAL_AUTH_FACEBOOK_KEY = '479693930159179'  # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = 'e146112ef931ae198c85b5c93b547fd8'  # App Secret
+
+db_name = 'ownway'
+db_id = 'rohhj622'
+db_pw = 'shguswls1!'
+
+# db_name = 'ownway'
+# db_id = 'onensix23'
+# db_pw = 'Epaktldk1!'
+
