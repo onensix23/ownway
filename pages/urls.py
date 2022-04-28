e40fde5c7ab24fa26808cd4672b8def503034844
rohhj622 <rohhj622@gmail.com>
2022-04-28 09:43:02 +0900

    resign page :220428

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('howtoresign', views.index, name='index2'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'test.html')
