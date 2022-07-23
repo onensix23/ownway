a6948ba25f413e82bf95fb6269f699c0d6be6191
rohhj622 <rohhj622@gmail.com>
2022-07-23 23:00:01 +0900

    main page :220723

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, redirect
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'test.html')
+    return render(request, 'main.html')
